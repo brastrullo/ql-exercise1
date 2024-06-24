205b08058989b60961f1f3d1de46399b31d71c27
Bradley Rastrullo <11504992+brastrullo@users.noreply.github.com>
2024-06-24 07:54:23 -0400

    Revert "simlab 3 exercise 1"
    This reverts commit bd7aaef181735f5439732b6270298f7d296271ba.

--- sample.c
+++ sample.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-    
-int main(int argc, char **argv) {
-    char *items[] = {"boat", "car", "truck", "train"};
-    int index = atoi(argv[1]);
-    printf("You selected %s\n", items[index-1]);
+#include <string.h>
+
+#define BUFSIZE 256
+
+// Function that escapes special characters in a given input string meant for a shell command.
+char* encode_shell_string(const char* str) {
+    size_t len = strlen(str);
+    char* escaped_str = malloc(len * 2 + 1); // allocate space for the escaped string
+
+    if (escaped_str == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t i, j;
+    for (i = 0, j = 0; i < len; i++) {
+        if (str[i] == '\'' || str[i] == '\\') {
+            escaped_str[j++] = '\\';
+        }
+        escaped_str[j++] = str[i];
+    }
+    escaped_str[j] = '\0';
+
+    return escaped_str;
+}
+
+// This program prints the size of a specified file in bytes
+int main(int argc, char** argv) {
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) { 
+        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        return -1;
+    }
+    char cmd[BUFSIZE] = "wc -c < ";
+    char* escaped_str = encode_shell_string(argv[1]);
+    strncat(cmd, escaped_str, BUFSIZE - 9);
+    free(escaped_str);
+    system(cmd);
 }
